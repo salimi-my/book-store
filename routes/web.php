6c8163a350ef914b286a84894e6288f78a1b7a51
Mohamad Salimi <contact@salimi.my>
2023-09-06 23:57:28 +0800

    Remove dashboard route

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,6 @@ Route::get('/', function () {
     return Inertia::render('Landing', []);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
