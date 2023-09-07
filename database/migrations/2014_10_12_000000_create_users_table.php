28c2a10a6cfc67c7f1bb7dd6d8d1b89c5297da93
Mohamad Salimi <contact@salimi.my>
2023-09-07 18:43:33 +0800

    Add avatar to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('avatar')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
