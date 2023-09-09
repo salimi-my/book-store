3ef870d841d4f3b009c350ecac25d427a40ba4a2
Mohamad Salimi <contact@salimi.my>
2023-09-09 16:56:33 +0800

    Update min height

--- resources/js/Layouts/ProfileLayout.jsx
+++ resources/js/Layouts/ProfileLayout.jsx
@@ -2,7 +2,7 @@ import ProfileCard from "@/Components/ProfileCard";
 
 export default function ProfileLayout({ children }) {
   return (
-    <section className="w-full bg-gray-50 dark:bg-gray-900">
+    <section className="min-h-[calc(100vh_-_64px_-_534px)] w-full bg-gray-50 dark:bg-gray-900">
       <div className="w-full max-w-6xl grid lg:grid-cols-10 gap-6 px-4 xl:mx-auto py-20">
         <div className="lg:col-span-3">
           <ProfileCard />
