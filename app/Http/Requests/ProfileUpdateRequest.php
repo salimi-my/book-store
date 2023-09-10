c2c96614d5492208dd188f8ded56f4aba8863664
Mohamad Salimi <contact@salimi.my>
2023-09-10 15:26:38 +0800

    Update avatar rule

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -19,7 +19,7 @@ class ProfileUpdateRequest extends FormRequest
             'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             'phone' => ['max:15'],
-            'avatar' => ['sometimes', 'mimes:jpg,png,jpeg,webp', 'max:5000']
+            'avatar' => 'nullable|mimes:jpg,png,jpeg,webp|max:5000'
         ];
     }
 
