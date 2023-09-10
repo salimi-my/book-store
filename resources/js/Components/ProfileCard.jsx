7b6d04accfbc894e1d2d832eeae80b4450afc3e4
Mohamad Salimi <contact@salimi.my>
2023-09-10 15:03:00 +0800

    Update profile menu

--- resources/js/Components/ProfileCard.jsx
+++ resources/js/Components/ProfileCard.jsx
@@ -22,34 +22,31 @@ export default function ProfileCard() {
         <div className="w-full px-8 mt-8">
           <Link
             href={route("profile.edit")}
-            as="button"
             className={`${
               pathname === "/profile"
                 ? "bg-cyan-700 text-white dark:text-white"
                 : ""
-            } w-full mb-2 text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-cyan-500 dark:text-cyan-500 dark:hover:text-white dark:hover:bg-cyan-500 dark:focus:ring-cyan-800`}
+            } flex justify-center w-full mb-2 text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-2 focus:outline-none focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-cyan-500 dark:text-cyan-500 dark:hover:text-white dark:hover:bg-cyan-500 dark:focus:ring-cyan-800`}
           >
             Profile
           </Link>
           <Link
             href={route("address.index")}
-            as="button"
             className={`${
               pathname === "/address"
                 ? "bg-cyan-700 text-white dark:text-white"
                 : ""
-            } w-full mb-2 text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-cyan-500 dark:text-cyan-500 dark:hover:text-white dark:hover:bg-cyan-500 dark:focus:ring-cyan-800`}
+            } flex justify-center w-full mb-2 text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-2 focus:outline-none focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-cyan-500 dark:text-cyan-500 dark:hover:text-white dark:hover:bg-cyan-500 dark:focus:ring-cyan-800`}
           >
             Addresses
           </Link>
           <Link
             href="#"
-            as="button"
             className={`${
               pathname === "/order"
                 ? "bg-cyan-700 text-white dark:text-white"
                 : ""
-            } w-full mb-2 text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-cyan-500 dark:text-cyan-500 dark:hover:text-white dark:hover:bg-cyan-500 dark:focus:ring-cyan-800`}
+            } flex justify-center w-full mb-2 text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-2 focus:outline-none focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-cyan-500 dark:text-cyan-500 dark:hover:text-white dark:hover:bg-cyan-500 dark:focus:ring-cyan-800`}
           >
             Orders
           </Link>
@@ -58,7 +55,7 @@ export default function ProfileCard() {
             href={route("logout")}
             method="post"
             as="button"
-            className="w-full my-2 text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-cyan-500 dark:text-cyan-500 dark:hover:text-white dark:hover:bg-cyan-500 dark:focus:ring-cyan-800"
+            className="w-full my-2 text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-2 focus:outline-none focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2 text-center dark:border-cyan-500 dark:text-cyan-500 dark:hover:text-white dark:hover:bg-cyan-500 dark:focus:ring-cyan-800"
           >
             <i className="fa-solid fa-right-from-bracket"></i> Sign out
           </Link>
