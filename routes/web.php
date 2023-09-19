398959d3fe019821731d5356fc4f201748fec8c2
Mohamad Salimi <contact@salimi.my>
2023-09-19 17:24:20 +0800

    Create resource cartcontroller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\AddressDefaultController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,6 +24,8 @@ Route::get('/', function () {
 });
 
 Route::resource('book', BookController::class)->only(['index', 'show']);
+
+Route::resource('cart', CartController::class)->only(['store']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
