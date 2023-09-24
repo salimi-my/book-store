8e5934eb9bb1282343b95c4aaf701852ef51baae
Mohamad Salimi <contact@salimi.my>
2023-09-24 17:28:20 +0800

    Add checkout item quantity

--- resources/js/Pages/Checkout/Partials/CheckoutItem.jsx
+++ resources/js/Pages/Checkout/Partials/CheckoutItem.jsx
@@ -14,12 +14,15 @@ export default function CheckoutItem({ cart }) {
     <div className="flex items-center space-x-2 md:space-x-4 text-sm">
       <Link
         href={route("book.show", cart.book_owner.slug)}
-        className="border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 p-1 rounded-md shrink-0 transition-transform ease-in-out hover:scale-105"
+        className="relative border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 p-1 rounded-md shrink-0 transition-transform ease-in-out hover:scale-105"
       >
         <img
           src={`/book-images/${cart.book_owner.image}`}
           className="w-16 h-[5.5rem] object-cover rounded-md"
         />
+        <div className="absolute -top-2 -right-2 flex justify-center items-center bg-cyan-600 dark:bg-cyan-500 w-6 h-6 rounded-full text-white font-medium">
+          {cart.quantity}
+        </div>
       </Link>
       <div className="w-full flex justify-between items-center text-gray-900 dark:text-white">
         <Link
