7d881a4006cf6b4cf4d2fd4ed5a5456b854bf3ae
Mohamad Salimi <contact@salimi.my>
2023-09-27 19:06:21 +0800

    Remove unuse functions

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -22,14 +22,6 @@ class OrderController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -177,14 +169,6 @@ class OrderController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Order $order)
@@ -235,13 +219,5 @@ class OrderController extends Controller
         } else {
             return abort(404);
         }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
